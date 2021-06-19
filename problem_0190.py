5a08155021f4f71cf88b911b9ed0eebaf2bfafbc
Marc Cornet <marccornet@gmail.com>
2021-06-19 08:53:37 +0200

    remove problem 190 garbage

--- problem_0190.py
+++ problem_0190.py
@@ -1,39 +1,6 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-
-        sign = 1 if x > 0 else -1 
-        reverse_number = 0
-        x = abs(x)
-
-        while x != 0:
-            # step 1: pop the last digit
-            last_digit = x % 10
-            x //= 10
-
-            # step 2: push to the result
-            reverse_number *= 10 
-            reverse_number += last_digit
-
-        if reverse_number > 0x7FFFFFFF:
-            return 0
-
-        return sign * reverse_number
-
-    
+class Solution:  
     def reverseBits(self, n: int) -> int:
-        # step 1, calculate the number from the input
-        number = 0
-        while n != 0:
-            
-            # step 1: pop the last digit
-            last_digit = n % 10
-            n //= 10
-            
-            # step 2: push to the result
-            number **= 2 
-            number += last_digit
-
-        return number
+        pass
 
 if __name__ == "__main__":
     c = Solution()

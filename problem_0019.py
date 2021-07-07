3a034e71ff6d3335df278fe0b5b0ce868d4a646f
Marc Cornet <marccornet@gmail.com>
2021-07-07 11:57:14 +0200

    p 19: try & except to solve edge case, beating 98% of solutions

--- problem_0019.py
+++ problem_0019.py
@@ -8,32 +8,70 @@ class ListNode:
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         """
-        Using 2 pointers with a gap of N
+        Using 2 pointers with a gap of n
 
-        When the first pointer reaches the end of the array
-        Skip the element by connecting the last.next to last.next.next instead
+        Move the fast pointer till it reaches the end of the array
+        Skip the nth element by connecting the slow.next to slow.next.next
+
+        Example remove n = 3 from the list
+
+        n:  6  5  4  3  2  1
+        l: [1, 2, 3, 4, 5, 6]
+
+        Step 1: set fast and slow:
+
+            *
+        n:  6  5  4  3  2  1    
+        l: [1, 2, 3, 4, 5, 6]
+            #
+
+        Step 2: create gap n
+
+                     *
+        n:  6  5  4  3  2  1    
+        l: [1, 2, 3, 4, 5, 6]
+            #        
+
+        Step 3: move both pointers till fast reaches end
+
+                           *
+        n:  6  5  4  3  2  1
+        l: [1, 2, 3, 4, 5, 6]
+                  #           
+
+        Step 4: set #.next to #.next.next
+                return head
         
+        Edge Case: n equals length of list, skip the first element
+
+        l: [1], n = 1
+
+
+        Step 3:
+
+        Example: [1], n = 1
+
+            # = None: the while loop will throw an exception
+        [1]
+         *
+            return head.next
         """     
-        # set 2 pointers, starting at the head
-        first = last = head
+        # Step 1: Init fast and slow
+        fast = slow = head
         
-        # create the gap
+        # Step 2: Create the gap
         for _ in range(n):
-            first = first.next
+            fast = fast.next
         
-        # Edge case detection: 
-        # n equals length of array
-        # asked to remove the first element in the array
-        # and first is already out of bounds for the while loop
-        if first is None:          
-            head = head.next
-            return head
-        
-        while first.next:
-            last = last.next
-            first = first.next
-            
-        # remove the n-th node by skipping it
-        last.next = last.next.next
-        
+        # Step 3: Move fast and slow till fast is at the end
+        try:       
+            while fast.next:
+                slow = slow.next
+                fast = fast.next
+        except:
+            # Edge case: n is list length, skip the first element
+            return head.next
+
+        # Step 4. remove the n-th node
+        slow.next = slow.next.next
         return head

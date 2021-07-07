ba0c14c8520bb84a7a03cccef8f0bb1e245f4886
Marc Cornet <marccornet@gmail.com>
2021-07-07 12:07:25 +0200

    p 19: try & except updated comments

--- problem_0019.py
+++ problem_0019.py
@@ -8,22 +8,29 @@ class ListNode:
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         """
-        Using 2 pointers with a gap of n
+        Challenge: To remove the n-th element from the end of the list
 
-        Move the fast pointer till it reaches the end of the array
-        Skip the nth element by connecting the slow.next to slow.next.next
+        Solution: Using the fast and slow pointer pattern.
 
-        Example remove n = 3 from the list
+        - The fast pointer will start with a gap of n away from slow.
+        
+        - Both pointers will move at the same speed till the fast pointer reaches 
+            the end of the list (fast.next is None)
+        
+        - Skip the nth element by connecting the slow.next to slow.next.next
+
+        Example:
+            remove n = 3 from a list
 
         n:  6  5  4  3  2  1
         l: [1, 2, 3, 4, 5, 6]
 
-        Step 1: set fast and slow:
+        Step 1: Start fast and slow at the head of the list:
 
-            *
+            * (fast)
         n:  6  5  4  3  2  1    
         l: [1, 2, 3, 4, 5, 6]
-            #
+            # (slow)
 
         Step 2: create gap n
 
@@ -33,28 +40,28 @@ class Solution:
             #        
 
         Step 3: move both pointers till fast reaches end
+            (while fast.next: move pointers)
+            the slow pointer is now at n-1
 
                            *
         n:  6  5  4  3  2  1
         l: [1, 2, 3, 4, 5, 6]
                   #           
 
-        Step 4: set #.next to #.next.next
-                return head
+        Step 4: set #.next (node 3) to #.next.next (node 5)
+            return the head of the adjusted list
         
-        Edge Case: n equals length of list, skip the first element
 
-        l: [1], n = 1
+        Edge Case: n equals length of list, 
+            Solution: skip the first element and return head.next
 
+        Example: l: [1], n = 1
 
-        Step 3:
+        After step 1 and 2 the pointers will look like:
 
-        Example: [1], n = 1
-
-            # = None: the while loop will throw an exception
+            # is None: the while loop will throw an exception
         [1]
          *
-            return head.next
         """     
         # Step 1: Init fast and slow
         fast = slow = head

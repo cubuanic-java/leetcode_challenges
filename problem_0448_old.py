2813a38f1d14c5fdec2ef950007f9bf96f08d5b4
Marc Cornet <marccornet@gmail.com>
2021-07-07 15:31:15 +0200

    Renaming and adding files

---
+++ problem_0448_old.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        missing = []
+        nums_set = set(nums)
+        for i in range(1, len(nums)+1):
+            if i not in nums_set:
+                missing.append(i)
+
+        return missing
+        

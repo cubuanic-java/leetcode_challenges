220161cba9af46562655af270d40d07b4a4065c0
Marc Cornet <marccornet@gmail.com>
2021-07-07 15:25:28 +0200

    448 bugfix - forgot abs() function

--- problem_0448.py
+++ problem_0448.py
@@ -1,11 +1,11 @@
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        
         missing = []
-
+        
         for n in nums:
-            if nums[n-1] > 0:
-                nums[n-1] *= -1
+            n_abs = abs(n)
+            if nums[n_abs-1] > 0:
+                nums[n_abs-1] *= -1
         
         for i in range(0,len(nums)):
             if nums[i] > 0:

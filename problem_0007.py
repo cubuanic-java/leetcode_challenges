6136b9bbd3b7b1a195bdbe5985981d934d18d2dd
Marc Cornet <marccornet@gmail.com>
2021-07-08 15:29:59 +0200

    more cleaning

--- problem_0007.py
+++ problem_0007.py
@@ -1,32 +1,23 @@
 class Solution:
     def reverse(self, x: int): # -> int:
         
-        #if x == 0: return 0
-        positive_input = True if x > 0 else False 
-
-        modulo_divisor = 10 if positive_input else -10
+        sign = 1 if x > 0 else -1 
         reverse_number = 0
+        x = abs(x)
 
-        while True:
-            # step 1: select an increasing number of digits with a modulo
-            digits_temp = x % modulo_divisor
-            # step 2: extract the first digit of the selection with an integer division
-            digit = digits_temp // (modulo_divisor // 10)
-            # step 3: add the digit to the number
-            reverse_number += digit
-            # end if the modulo division equals the starting number
-            if digits_temp == x: 
-                if not positive_input: 
-                    reverse_number *= -1
-                
-                if reverse_number < 2**31 -1 and reverse_number > -2**31:
-                    return reverse_number
-                else:
-                    return 0
-            # continue to next 10 fold
-            reverse_number *= 10
-            modulo_divisor *= 10
+        while x != 0:
+            # step 1: pop the last digit
+            last_digit = x % 10
+            x //= 10
 
+            # step 2: push to the result
+            reverse_number *= 10 
+            reverse_number += last_digit
+
+        if reverse_number > 0x7FFFFFFF:
+            return 0
+
+        return sign * reverse_number
 
 
 if __name__ == "__main__":

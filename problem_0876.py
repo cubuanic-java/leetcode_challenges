ae1ff157c959ed1342097aabaf9f7c2a6fd90200
Marc Cornet <marccornet@gmail.com>
2021-07-09 08:18:26 +0200

    876 cleanup

--- problem_0876.py
+++ problem_0876.py
@@ -4,62 +4,74 @@ class ListNode:
          self.val = val
          self.next = next
 class Solution:
+    """
+    Fast and Slow pointers / Floyd's algorithm
+    
+    A single linked list is like a racetrack
+    
+    The faster pointer moving twice the speed
+    will terminate while the slow pointer is still in the middle
+    
+    While solving this the order in which we move the pointers is important.
+    But why does the try except work in this order for even sized arrays??
+
+    Example odd array
+
+    [1, 2, 3, 4, 5]
+
+    Iteration 1:
+    
+        #
+    [1, 2, 3, 4, 5]
+        *
+    
+    Iteration 2:
+    
+                #
+    [1, 2, 3, 4, 5]
+        *
+    
+    Iteration 3:
+                    #
+    [1, 2, 3, 4, 5]
+            *
+    
+    # is None now, next iteration will fail with * in the correct position
+    
+    Iteration 1:
+    
+        #
+    [1, 2, 3, 4]
+        *
+        
+    Iteration 2:
+    
+                #
+    [1, 2, 3, 4]
+        *
+        
+    Iteration 3:
+    
+                _ # the fast pointer is None before the slow.next line?
+    [1, 2, 3, 4]
+        * 
+    
+    Why does the try still execute the slow.next line?
+    """
+
+    # Initial solution
     def middleNode(self, head: ListNode) -> ListNode:
-        """
-        Floyd's algorithm
-        
-        A single linked list is like a racetrack
-        
-        The faster pointer moving twice the speed
-        will terminate while the slow pointer is still in the middle
-        
-        While solving this the order in which we move the pointers is important.
-        But why does the try except work in this order for even sized arrays??
+        slow = fast = head
 
-        Example odd array
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        [1, 2, 3, 4, 5]
+        return slow
 
-        Iteration 1:
-        
-            #
-        [1, 2, 3, 4, 5]
-         *
-        
-        Iteration 2:
-        
-                  #
-        [1, 2, 3, 4, 5]
-            *
-        
-        Iteration 3:
-                       #
-        [1, 2, 3, 4, 5]
-               *
-        
-        # is None now, next iteration will fail with * in the correct position
-        
-        Iteration 1:
-        
-            #
-        [1, 2, 3, 4]
-         *
-         
-        Iteration 2:
-        
-                  #
-        [1, 2, 3, 4]
-            *
-            
-        Iteration 3:
-        
-                    _ # the fast pointer is None before the slow.next line?
-        [1, 2, 3, 4]
-            * 
-        
-        Why does the try still execute the slow.next line?
-        """
-        
+
+    # Try Except solution
+    def middleNode(self, head: ListNode) -> ListNode:   
         slow = fast = head
         
         try:
@@ -68,14 +80,3 @@ class Solution:
                 slow = slow.next
         except:
             return slow
-        
-
-
-    def middleNode(self, head: ListNode) -> ListNode:
-
-        slow = fast = head
-
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow

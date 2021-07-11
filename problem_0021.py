01a7fdb76067b961b8fbf4a8a42b7b721f58963a
Marc Cornet <marccornet@gmail.com>
2021-07-11 10:58:59 +0200

    Cleaner 21, Solved 88

--- problem_0021.py
+++ problem_0021.py
@@ -1,3 +1,10 @@
+"""
+21. Merge Two Sorted Lists
+
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+Classification: Two Pointer
+"""
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -6,59 +13,37 @@ class ListNode:
 
 
 class Solution:
+
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         """
-        This will merge two lists.
+        ## Challenge
 
-        Step 1. Start and head are None at the start.
-                Since head will move during a merge process 
-                return the starting pointer instead.
-        Step 2. Move the head over the lists in increasing values
-                This means we need to keep a pointer to the starting point
-                to return at the end of the function
-        Step 3. Take care of the leftover values from one of the lists
-        Step 4. Return the start
+            Merge two sorted linked lists and return it as a sorted list. 
+            The list should be made by splicing together the nodes of the first two lists.
+
+        ## Solution
+
+            Use a new starting head pointing to None
+
+            1. Splice while both lists still hold values into a new head pointer
+            2. Add the remaining values
+            3. Return start.next
         """         
-        start = head = None
+        start = ListNode(0, None)
+        head = start
         
-        # Step 1.
-        # If both lists exists at the start pick a starting point
-        if l1 and l2:
-            # print(f'two lists exist, l1: {l1}, l2: {l2}')
-            if l1.val < l2.val:
-                # print(f'l1 val: {l1.val} and l2 val: {l2.val}, picking l1 as the start')
-                start = head = l1
-                l1 = l1.next
-            else:
-                start = head = l2
-                l2 = l2.next
-
-        # Step 2.
-        # Merge while both lists still hold values    
+        # Splicing
         while l1 and l2:
             if l1.val < l2.val:
-                head.next = l1      # assign the value
-                l1 = l1.next        # move the list pointer, this might set l1 to None
+                head.next = l1      
+                l1 = l1.next        
             else:
                 head.next = l2
                 l2 = l2.next
-
-            head = head.next        # move the head pointer (one of l1 or l2)
+            head = head.next        
         
-        # STEP 3.
-        # 2 possible scenarios we are here:
-        # - Leftover values after merging 
-        # - one list was empty from the start
-        if l1:
-            if start == None: return l1  # only one list so start was never set
-            head.next = l1
-        
-        if l2:
-            if start == None: return l2
-            head.next = l2
+        # Remaininging Values
+        if l1: head.next = l1
+        if l2: head.next = l2
 
-        # Step 4.
-        # if both lists existed a start node was set at the initiliaze part
-        # if only one list existed that listNode was returned in Step 3.
-        # if no list existed this should be an empty node
-        return start
+        return start.next
--- readme.md
+++ readme.md
@@ -14,9 +14,12 @@ https://seanprashad.com/leetcode-patterns/
 - 0488: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 - 0136: https://leetcode.com/problems/single-number/ (Bit Manipulation, XOR)
 - 0001: https://leetcode.com/problems/two-sum/
+- 0088: https://leetcode.com/problems/merge-sorted-array/ (Arrays, Two Pointers)
 
 
 ## Fast & Slow Pointers
 
 ## Two Pointers
+
+- 0021: https://leetcode.com/problems/merge-two-sorted-lists/
 

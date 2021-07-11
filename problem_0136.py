c51d0f7a0d6bf416cc0dc469d9fc4eee3972b7f4
Marc Cornet <marccornet@gmail.com>
2021-07-11 07:51:49 +0200

    list update

---
+++ problem_0136.py
@@ -0,0 +1,59 @@
+"""
+136. Single Number
+
+https://leetcode.com/problems/single-number/
+
+Classification: Array, Bit Manipulation
+"""
+
+class Solution:
+    """
+    ## Challenge:
+
+        Given a non-empty array of integers nums, 
+        every element appears twice except for one. 
+        Find that single one.
+
+        You must implement a solution with a linear runtime 
+        complexity and use only constant extra space.
+    
+    ## Solutions:
+
+        1. Naive: 
+
+            Sort the array (n log n)
+            Find the missing number
+
+        2. Bit Manipulation (XOR)
+
+            https://en.wikipedia.org/wiki/Bitwise_operation#XOR
+
+            - x ^ x = 0
+            - x ^ x ^ y = y
+
+    """
+
+    def singleNumber(self, nums: list[int]) -> int:
+        """
+        Approach 1: sort the array and loop over the array in pairs
+        """
+        nums = sorted(nums)
+        
+        for index in range(0, len(nums)-1, 2):
+            if nums[index] != nums[index + 1]:
+                return nums[index]
+        
+        # in case it was the last number
+        return nums[-1]
+
+
+    def singleNumber(self, nums: list[int]) -> int:
+        """
+        XOR Approach
+        """
+        
+        res = 0
+        for n in nums:
+            res ^= n 
+        
+        return res
--- readme.md
+++ readme.md
@@ -9,10 +9,11 @@ https://seanprashad.com/leetcode-patterns/
 
 ## Arrays
 
-- Problem 0217: https://leetcode.com/problems/contains-duplicate/
-- Problem 0268: https://leetcode.com/problems/missing-number/ (Bit Manipulation)
-- Problem 0488: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
-- Problem 0001: https://leetcode.com/problems/two-sum/
+- 0217: https://leetcode.com/problems/contains-duplicate/
+- 0268: https://leetcode.com/problems/missing-number/ (Bit Manipulation, Sum first n integers)
+- 0488: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+- 0136: https://leetcode.com/problems/single-number/ (Bit Manipulation, XOR)
+- 0001: https://leetcode.com/problems/two-sum/
 
 
 ## Fast & Slow Pointers

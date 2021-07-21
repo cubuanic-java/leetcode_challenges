a2e57ff001061d58cf3aae5a3ceac66b5cb027dd
Marc Cornet <marccornet@gmail.com>
2021-07-21 07:13:11 +0200

    p53 max subarray

--- problem_0053.py
+++ problem_0053.py
@@ -17,6 +17,9 @@
 
 """
 class Solution:
+    """
+    Based on chapter 22 from Rossi
+    """
     def maxSubArray(self, nums: list[int]) -> int:
         if not nums: return 0
 
@@ -35,3 +38,35 @@ class Solution:
             prev_chunk = chunk
 
         return best_chunk
+
+
+class Solution:
+    """
+    Simplify the negative check and initialize the values at nums[0]
+    """
+    def maxSubArray(self, nums: list[int]) -> int:
+        prev_chunk = best_chunk = nums[0]
+
+        for num in nums[1:]:
+            if prev_chunk < 0 : prev_chunk = 0
+            prev_chunk = prev_chunk + num     
+            if best_chunk < prev_chunk: best_chunk = prev_chunk
+
+        return best_chunk
+
+
+class Solution:
+    """
+    A call to max is also used a lot on leetcode.
+    This works because the next positive value will become the new
+    current_chunk when a chunk became negative in the previous run
+    """
+    def maxSubArray(self, nums: list[int]) -> int:
+        best_chunk = prev_chunk = nums[0]
+        for num in nums[1:]:
+            prev_chunk = max(prev_chunk + num, num)       
+            if best_chunk < prev_chunk: best_chunk = prev_chunk
+
+        return best_chunk
+
+

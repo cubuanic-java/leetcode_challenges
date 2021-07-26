9a34c65bc433ae97817ca0d262686ea805d9496c
Marc Cornet <marccornet@gmail.com>
2021-07-26 13:38:44 +0200

    file renames

--- Python/problem_0190.py
+++
@@ -1,9 +0,0 @@
-class Solution:  
-    def reverseBits(self, n: int) -> int:
-        pass
-
-if __name__ == "__main__":
-    c = Solution()
-    t1 = c.reverseBits(10100101000001111010011100)
-    print(t1)
-

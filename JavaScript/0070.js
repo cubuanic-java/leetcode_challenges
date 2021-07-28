7ebd7e692f9101cfaddee6ce9bd57586210df380
Marc Cornet <marccornet@gmail.com>
2021-07-28 10:19:34 +0200

    p70 wrong code

--- JavaScript/0070.js
+++ JavaScript/0070.js
@@ -2,17 +2,17 @@
  * @param {number} n
  * @return {number}
  */
- var climbStairs = function(n) {
+var climbStairs = function(n) {
     
-    let dp_cache = new Array(n+1);
+    let dp_cache = new Array(n+2);
     // base cases
-    dp_cache[0] = 1;
-    dp_cache[1] = 2;
+    dp_cache[0] = 0;
+    dp_cache[1] = 1;
     
-    for(let i = 2; i < n; i++) 
+    for(let i = 2; i <= n+1; i++) 
     {
         dp_cache[i] = dp_cache[i-1] + dp_cache[i-2]
     }
 
-    return dp_cache[n];
+    return dp_cache[n+1];
 };
